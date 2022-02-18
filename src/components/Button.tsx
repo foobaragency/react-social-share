395ad196973824927a79cf5009a1cc976254a66d
Sam Odum <samuel@foobar.agency>
2022-02-18 22:24:58 +0100

    Create base button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from 'react';
 
-export interface IButtonProps
-    extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {}
+import styles from './button.module.css';
+
+export type IButtonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 
 export const Button: FC<IButtonProps> = (props) => {
-    const { children } = props;
-
-    return <button {...props}>{children}</button>;
+    return (
+        <button {...props} className={styles.button}>
+            {props.children}
+        </button>
+    );
 };
