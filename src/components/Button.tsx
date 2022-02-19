de9927eb4d5c2a99b7df5b44e4fc6da855e5a18c
Sam Odum <samuel@foobar.agency>
2022-02-19 21:34:13 +0100

    Create share and composed share buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,33 @@
 import React, { FC } from 'react';
+import { useShare, shareGroupKey } from '../hooks/useShare';
 
 import styles from './button.module.css';
 
-export type IButtonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
+interface IShareButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+    domain: shareGroupKey;
+    url: string;
+    subject?: string;
+}
 
-export const Button: FC<IButtonProps> = (props) => {
+interface IComposedProps {
+    url?: string;
+    subject?: string;
+}
+
+export const ShareButton: FC<IShareButtonProps> = (props) => {
+    const { children, domain, url, subject } = props;
+    const { target } = useShare();
+
     return (
-        <button {...props} className={styles.button}>
-            {props.children}
+        <button {...props} className={styles.button} onClick={() => target(domain, url, subject)}>
+            {children}
         </button>
     );
 };
+
+export const Print: FC<IComposedProps> = () => <ShareButton domain="print" url="nothing" />;
+export const WhatsApp: FC<IComposedProps> = ({ url = window.location.href }) => <ShareButton domain="whatsapp" url={url} />;
+export const Twitter: FC<IComposedProps> = ({ url = window.location.href }) => <ShareButton domain="twitter" url={url} />;
+export const facebook: FC<IComposedProps> = ({ url = window.location.href }) => <ShareButton domain="facebook" url={url} />;
+export const pinterest: FC<IComposedProps> = ({ url = window.location.href }) => <ShareButton domain="pinterest" url={url} />;
+export const Email: FC<IComposedProps> = ({ url = window.location.href, subject }) => <ShareButton domain="email" url={url} subject={subject} />;
